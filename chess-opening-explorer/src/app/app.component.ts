8950df6744fbc975a771e4787d088bc40d8cdae4
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-07-08 22:46:46 +0300

    configured angular/fire and firebase db

--- chess-opening-explorer/src/app/app.component.ts
+++ chess-opening-explorer/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'chess-opening-explorer';
+
+  constructor(private db: AngularFireDatabase) {}
+
+  getAllData() {
+    return this.db.list('/').valueChanges();
+  }
+
+  ngOnInit() {
+    this.getAllData().subscribe((data: any) => {
+      console.log(data);
+    });
+  }
 }
