582f06a427dea1fa93ef3b9dee9a64de7586189c
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-07-10 22:17:18 +0300

    add handlers for reset/undo/flip board

--- chess-opening-explorer/src/app/opening/opening-details/opening-details.component.ts
+++ chess-opening-explorer/src/app/opening/opening-details/opening-details.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { NgxChessBoardService } from 'ngx-chess-board';
+import { NgxChessBoardView } from 'ngx-chess-board';
 
 @Component({
   selector: 'app-opening-details',
@@ -9,4 +11,20 @@ import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 export class OpeningDetailsComponent {
   faArrowLeft = faArrowLeft;
   faArrowRight = faArrowRight;
+
+  @ViewChild('board', { static: false }) board!: NgxChessBoardView;
+
+  constructor(private ngxChessBoardService: NgxChessBoardService) {}
+
+  resetBoardHandler(): void {
+    this.board.reset();
+  }
+
+  flipBoardHandler(): void {
+    this.board.reverse();
+  }
+
+  undoMoveHandler(): void {
+    this.board.undo();
+  }
 }
