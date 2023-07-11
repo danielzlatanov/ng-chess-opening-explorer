4bf57a20758c2c5dbad588dc700d738a779f98ee
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-07-11 18:20:15 +0300

    set a dynamic title for each page

--- chess-opening-explorer/src/app/app.component.ts
+++ chess-opening-explorer/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { AngularFireDatabase } from '@angular/fire/compat/database';
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivationStart, Router } from '@angular/router';
+import { filter, map } from 'rxjs';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,11 +12,25 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'chess-opening-explorer';
 
-  constructor(private db: AngularFireDatabase) {}
-
-  getAllData() {
-    return this.db.list('/').valueChanges();
+  constructor(
+    private db: AngularFireDatabase,
+    private router: Router,
+    private pageTitle: Title
+  ) {
+    this.router.events
+      .pipe(
+        filter((e): e is ActivationStart => e instanceof ActivationStart),
+        map((e) => e.snapshot.data?.['title']),
+        filter((x) => Boolean(x))
+      )
+      .subscribe((pageTitle) => {
+        this.pageTitle.setTitle(pageTitle);
+      });
   }
+
+  // getAllData() {
+  //   return this.db.list('/').valueChanges();
+  // }
 
   ngOnInit() {}
 }
