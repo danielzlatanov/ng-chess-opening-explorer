0127da80990532585f7d0e9b884db26bc0a0a2c2
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-07-14 14:09:59 +0300

    catalog cmp renders available openings

--- chess-opening-explorer/src/app/opening/opening-catalog/opening-catalog.component.ts
+++ chess-opening-explorer/src/app/opening/opening-catalog/opening-catalog.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { OpeningService } from '../opening.service';
+import { IOpening } from 'src/app/shared/interfaces/opening';
 
 @Component({
   selector: 'app-opening-catalog',
   templateUrl: './opening-catalog.component.html',
-  styleUrls: ['./opening-catalog.component.scss']
+  styleUrls: ['./opening-catalog.component.scss'],
 })
 export class OpeningCatalogComponent {
+  openings: IOpening[] | null = [];
 
+  constructor(private openingService: OpeningService) {}
+
+  ngOnInit(): void {
+    this.openingService
+      .getAllOpenings()
+      .then((openings) => {
+        this.openings = openings;
+        // console.log('all openings received: ', this.openings);
+        // this.changeDetectorRef.detectChanges();
+      })
+      .catch((err) => {
+        this.openings = null;
+        console.error('Error fetching all openings: ', err.message);
+      });
+  }
 }
