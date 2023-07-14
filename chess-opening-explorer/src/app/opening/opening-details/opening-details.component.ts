d67442a688dd7a169d1e8aecd092e36b16c70fa7
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-07-14 20:12:08 +0300

    implement dynamic rendering of opening details

--- chess-opening-explorer/src/app/opening/opening-details/opening-details.component.ts
+++ chess-opening-explorer/src/app/opening/opening-details/opening-details.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { OpeningService } from '../opening.service';
+import { ActivatedRoute } from '@angular/router';
+import { IOpening } from 'src/app/shared/interfaces/opening';
 
 @Component({
   selector: 'app-opening-details',
   templateUrl: './opening-details.component.html',
   styleUrls: ['./opening-details.component.scss'],
 })
-export class OpeningDetailsComponent {
+export class OpeningDetailsComponent implements OnInit {
   faArrowLeft = faArrowLeft;
   faArrowRight = faArrowRight;
+  openingId!: string;
+  opening: IOpening | null = null;
+
+  constructor(
+    private openingService: OpeningService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.openingId = this.route.snapshot.paramMap.get('id') as string;
+    this.fetchOpeningDetails();
+  }
+
+  fetchOpeningDetails(): void {
+    this.openingService
+      .getOpeningById(this.openingId)
+      .then((opening) => {
+        this.opening = opening;
+      })
+      .catch((err) => {
+        this.opening = null;
+        console.error('Error fetching current opening: ', err.message);
+      });
+  }
 }
