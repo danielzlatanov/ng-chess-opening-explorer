7eb1e2e127878d10c68f2fbbd3c3b12d0b3e78f7
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-07-14 20:06:03 +0300

    implement fetching opening details based on url id

--- chess-opening-explorer/src/app/opening/opening.service.ts
+++ chess-opening-explorer/src/app/opening/opening.service.ts
@@ -46,6 +46,27 @@ export class OpeningService {
     return this.fetchOpenings(true);
   }
 
+  getOpeningById(id: string): Promise<IOpening> {
+    return new Promise<IOpening>((resolve, reject) => {
+      this.openingsRef.query
+        .orderByKey()
+        .equalTo(id)
+        .once('value')
+        .then((snapshot) => {
+          const openingSnapshot = snapshot.val();
+
+          if (openingSnapshot) {
+            const openingId = Object.keys(openingSnapshot)[0];
+            const opening = openingSnapshot[openingId];
+            resolve({ id: openingId, ...opening });
+          } else {
+            reject(new Error(`Opening could not be found`));
+          }
+        })
+        .catch((error) => reject(error));
+    });
+  }
+
   createOpening(opening: IOpening): Promise<void> {
     return new Promise<void>((resolve, reject) => {
       this.openingsRef
