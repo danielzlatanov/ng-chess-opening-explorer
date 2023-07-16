99687a358cd7f0a1ae1aa2fd06aa9e9ce53fc236
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-07-16 12:43:20 +0300

    display dynamic personal info in profile component

--- chess-opening-explorer/src/app/auth/profile/profile.component.ts
+++ chess-opening-explorer/src/app/auth/profile/profile.component.ts
@@ -1,19 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   faChessKnight,
   faStar,
   faUser,
   faChessBoard,
 } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from '../auth.service';
+import { User } from 'firebase/auth';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
   faUser = faUser;
   faChessKnight = faChessKnight;
   faChessBoard = faChessBoard;
   faStar = faStar;
+  user: User | null = null;
+  euTime: string | undefined = undefined;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.user$.subscribe((user) => {
+      this.user = user;
+      const creationTimeStr = this.user?.metadata.creationTime;
+      const creationTime = creationTimeStr ? new Date(creationTimeStr) : null;
+
+      this.euTime = creationTime?.toLocaleString('en-US', {
+        timeZone: 'Europe/Sofia',
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true,
+      });
+    });
+  }
 }
