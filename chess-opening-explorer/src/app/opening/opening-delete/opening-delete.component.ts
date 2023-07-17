7e5dffdac203d2cb4a3970d891f369f7eb702135
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-07-17 17:57:00 +0300

    opening delete cmp implements delete functionality

--- chess-opening-explorer/src/app/opening/opening-delete/opening-delete.component.ts
+++ chess-opening-explorer/src/app/opening/opening-delete/opening-delete.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { OpeningService } from '../opening.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-opening-delete',
   templateUrl: './opening-delete.component.html',
-  styleUrls: ['./opening-delete.component.scss']
+  styleUrls: ['./opening-delete.component.scss'],
 })
 export class OpeningDeleteComponent {
+  openingId!: string;
 
+  constructor(
+    private openingService: OpeningService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.openingId = this.route.snapshot.paramMap.get('id') as string;
+    this.openingService
+      .deleteOpening(this.openingId)
+      .then(() => {
+        console.log('Opening deleted successfully');
+        this.router.navigate(['/openings/catalog']);
+      })
+      .catch((err) => {
+        console.error('Error deleting opening: ', err.message);
+      });
+  }
 }
