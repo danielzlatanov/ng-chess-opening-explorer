704404162a88ac8f2673cb4ef7f698c25b5a72ba
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-07-28 09:29:55 +0300

    add notifications for redirect cases in auth guard

--- chess-opening-explorer/src/app/core/guards/auth.guard.ts
+++ chess-opening-explorer/src/app/core/guards/auth.guard.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Router, UrlTree } from '@angular/router';
 import { AuthService } from 'src/app/auth/auth.service';
+import { NotificationService } from 'src/app/shared/components/notification/notification.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthActivate {
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private notifService: NotificationService
+  ) {}
 
   async canActivate(route: ActivatedRouteSnapshot): Promise<boolean | UrlTree> {
     const loginRequired = route.data['loginRequired'];
@@ -20,8 +25,20 @@ export class AuthActivate {
       return true;
     }
 
+    this.notifService.showNotification('Please login first!');
+
     return this.authService.isLoggedIn
-      ? this.router.parseUrl('/openings/catalog')
-      : this.router.parseUrl('/auth/login');
+      ? this.redirectToCatalog()
+      : this.redirectToLogin();
+  }
+
+  redirectToCatalog(): UrlTree {
+    this.notifService.showNotification('You are already logged in.');
+    return this.router.parseUrl('/openings/catalog');
+  }
+
+  redirectToLogin(): UrlTree {
+    this.notifService.showNotification('Please sign in first.');
+    return this.router.parseUrl('/auth/login');
   }
 }
