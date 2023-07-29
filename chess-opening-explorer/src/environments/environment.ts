d7733c2cb0317e5c6a87e0e646c3827aec8df129
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-07-29 20:16:41 +0300

    unignore environment.ts & minor changes

---
+++ chess-opening-explorer/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'chess-opening-explorer',
+    appId: '1:104196082905:web:6e3139544e97583fe842c8',
+    databaseURL:
+      'https://chess-opening-explorer-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'chess-opening-explorer.appspot.com',
+    apiKey: 'AIzaSyA8Xca6SphNf4VihBI50zSJ4uJpA2O5Sm0',
+    authDomain: 'chess-opening-explorer.firebaseapp.com',
+    messagingSenderId: '104196082905',
+  },
+};
